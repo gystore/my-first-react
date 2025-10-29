e61afa13d6f6ddb6b43040d93387f84b49198f7f
gayeon <gystore85@gmail.com>
2025-10-29 13:01:51 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Hello from './Hello';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>나의 첫 React App</h1>
+      <Hello name="First" />
+      <Hello name="Second" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Hello.js
@@ -0,0 +1,5 @@
+function Hello({ name }) {
+    return <h2>안녕, {name}!</h2>
+}
+
+export default Hello;
